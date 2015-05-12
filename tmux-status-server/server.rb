ff990e5828c8b93f021a18b51280bf4409a2c44b
Roman Le Négrate <roman.lenegrate@gmail.com>
2015-05-12 10:29:39 -0400

    properly shorten commands

--- tmux-status-server/server.rb
+++ tmux-status-server/server.rb
@@ -3,25 +3,37 @@ require 'values_server'
 module TmuxStatus
   PORTS_RANGES = [8000..8999, 3000..3999]
 
-  def self.cpu
-    `ps -U #{Process.uid} -e -o %cpu,comm`.
-      split("\n").
-      drop(1).
-      map { |line| line.split(" ", 2) }.
-      sort_by { |pct,| -pct.to_f }[0,3].
-      map { |pct, cmd| "#{pct} #{File.basename(cmd)[0,7]}".rstrip }.
-      join(", ")
-  end
+  class << self
+    def cpu
+      `ps -U #{Process.uid} -e -o %cpu,comm`.
+        split("\n").
+        drop(1).
+        map { |line| line.split(" ", 2) }.
+        sort_by { |pct,| -pct.to_f }[0,3].
+        map { |pct, cmd| "#{pct} #{short_cmd(cmd)[0,7]}".rstrip }.
+        join(", ")
+    end
 
-  def self.ports
-    `lsof -l -P -n -i TCP -s TCP:LISTEN`.
-      split("\n").
-      drop(1).
-      grep(/.*:(\d+) \(LISTEN\)/) { $1.to_i }.
-      uniq.
-      select { |port| PORTS_RANGES.any? { |r| r.include? port } }.
-      sort.
-      join(", ")
+    def ports
+      `lsof -l -P -n -i TCP -s TCP:LISTEN`.
+        split("\n").
+        drop(1).
+        grep(/.*:(\d+) \(LISTEN\)/) { $1.to_i }.
+        uniq.
+        select { |port| PORTS_RANGES.any? { |r| r.include? port } }.
+        sort.
+        join(", ")
+    end
+
+  private
+
+    def short_cmd(cmd)
+      if cmd[0] != ?/
+        return cmd
+      end
+      exe, rest = cmd.split(" ", 2)
+      [File.basename(exe), rest].compact.join(" ")
+    end
   end
 end
 
